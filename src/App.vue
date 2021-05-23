68f13c90e1d6c83b15b0750f1e4f0b5af8998f8c
JeanSantana <jperez@xpertcode.com.do>
2021-05-23 14:46:13 -0400

    master: - Formatting vue files - Renaming method to standard convention

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div class="antialiased mx-auto max-w-screen-sm">
       <div class="mt-4 flex items-center inline-block">
       <h3 class="mb-4 text-lg font-semibold text-gray-900">Comments</h3>
-      <span @click="sortArray" class="mb-4 text-lg font-semibold ml-auto text-gray-900">
+      <span @click="commentsSorting" class="mb-4 text-lg font-semibold ml-auto text-gray-900">
         <svg id="Layer" enable-background="new 0 0 64 64" height="25" viewBox="0 0 64 64" width="25" xmlns="http://www.w3.org/2000/svg"><path d="m31.414 15.586-7-7c-.78-.781-2.048-.781-2.828 0l-7 7c-.781.781-.781 2.047 0 2.828.78.781 2.048.781 2.828 0l3.586-3.586v39.172c0 1.104.896 2 2 2s2-.896 2-2v-39.172l3.586 3.586c.39.391.902.586 1.414.586s1.024-.195 1.414-.586c.781-.781.781-2.047 0-2.828z"/><path d="m49.414 45.586c-.781-.781-2.047-.781-2.828 0l-3.586 3.586v-39.172c0-1.104-.896-2-2-2s-2 .896-2 2v39.172l-3.586-3.586c-.781-.781-2.048-.781-2.828 0-.781.781-.781 2.047 0 2.828l7 7c.391.391.902.586 1.414.586s1.023-.195 1.414-.586l7-7c.781-.781.781-2.047 0-2.828z"/></svg>
       </span>
       </div>
@@ -45,7 +45,7 @@ export default {
       comment.replies = []
       this.comments.push(comment);
     },
-    sortArray() {
+    commentsSorting() {
      this.comments= this.comments.sort( (a,b) => {
        return this.isSorted ?new Date(a.time) - new Date(b.time):new Date(b.time) - new Date(a.time) ;
       });
--- src/components/Comment/Comment.vue
+++ src/components/Comment/Comment.vue
@@ -8,17 +8,7 @@
       />
     </div>
     <div
-        class="
-              flex-1
-              border
-              rounded-lg
-              px-4
-              py-2
-              sm:px-6
-              sm:py-4
-              leading-relaxed
-            "
-    >
+        class=" flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
       <strong>{{comment.author}}</strong>
       <span class="text-xs text-gray-400 ml-2"> {{ comment.time }}</span>
       <input v-if="comment.isEditing" @keyup.enter="comment.isEditing = !comment.isEditing" v-model="comment.text" class=" text-sm bg-gray-100  rounded border border-gray-400 leading-normal  w-full h-10 py-2 px-3 font-medium placeholder-gray-500 focus:outline-none focus:bg-white" name="body" placeholder='Type Your Comment'/>
@@ -37,15 +27,7 @@
 
       <h4 v-if="!comment.hideReplies"
           @click="comment.hideReplies = !comment.hideReplies"
-          class="
-                my-5
-                uppercase
-                tracking-wide
-                text-gray-400
-                font-bold
-                text-xs
-              "
-      >
+          class=" my-5 uppercase tracking-wide text-gray-400 font-bold text-xs">
         Replies
       </h4>
       <div v-if="!comment.hideReplies" class="space-y-4">
